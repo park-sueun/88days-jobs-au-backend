0f80f31e14ad2df6b5ea15b5111d6549aa0006bf
Park, Su Eun <park_sueun@naver.com>
2026-02-26 13:49:43 +0800

    initialize Spring Boot project

---
+++ src/main/java/com/eightyeightdays/jobs_au_backend/JobsAuBackendApplication.java
@@ -0,0 +1,13 @@
+package com.eightyeightdays.jobs_au_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobsAuBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobsAuBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eightyeightdays/jobs_au_backend/JobsAuBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eightyeightdays.jobs_au_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobsAuBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
