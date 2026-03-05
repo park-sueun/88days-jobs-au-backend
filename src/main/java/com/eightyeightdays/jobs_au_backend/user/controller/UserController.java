5e2cfc1f9eb912ba1ba1412d720049117875911d
Park, Su Eun <park_sueun@naver.com>
2026-03-05 21:22:34 +0800

    implement User Query Service

--- src/main/java/com/eightyeightdays/jobs_au_backend/user/controller/UserController.java
+++ src/main/java/com/eightyeightdays/jobs_au_backend/user/controller/UserController.java
@@ -1,18 +1,34 @@
 package com.eightyeightdays.jobs_au_backend.user.controller;
 
+import com.eightyeightdays.jobs_au_backend.auth.model.CustomUserDetails;
+import com.eightyeightdays.jobs_au_backend.user.dto.UserResponse;
 import com.eightyeightdays.jobs_au_backend.user.service.UserService;
-import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.Mapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/users")
 public class UserController {
 
-    private UserService userService;
+    private final UserService userService;
+
+    @GetMapping
+    public List<UserResponse> getUsers(
+            @AuthenticationPrincipal CustomUserDetails user
+    ) {
+        return userService.getUsers();
+    }
+
+    @GetMapping("/{id}")
+    public UserResponse getUserById(
+            @AuthenticationPrincipal CustomUserDetails user,
+            @PathVariable Long id
+    ) {
+        return userService.getUser(id);
+    }
 
 }
