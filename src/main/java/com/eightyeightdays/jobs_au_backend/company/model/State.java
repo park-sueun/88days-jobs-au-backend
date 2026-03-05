ba126909e9dc352b376213224423c16159ffb70c
Park, Su Eun <park_sueun@naver.com>
2026-03-05 20:24:00 +0800

    define enum for Company

---
+++ src/main/java/com/eightyeightdays/jobs_au_backend/company/model/State.java
@@ -0,0 +1,12 @@
+package com.eightyeightdays.jobs_au_backend.company.model;
+
+public enum State {
+    NSW,
+    VIC,
+    WA,
+    SA,
+    QLD,
+    TAS,
+    NT,
+    ACT
+}
---
+++ src/main/java/com/eightyeightdays/jobs_au_backend/company/model/WorkType.java
@@ -0,0 +1,7 @@
+package com.eightyeightdays.jobs_au_backend.company.model;
+
+public enum WorkType {
+    FARM,
+    FACTORY,
+    HOSPITALITY
+}
