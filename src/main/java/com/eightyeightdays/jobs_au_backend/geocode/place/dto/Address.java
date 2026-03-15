87977db502915dd4ce92e8dd6e177a966961cbee
Park, Su Eun <park_sueun@naver.com>
2026-03-15 17:05:05 +0800

    fix: parse address from end to support multi-word suburb

--- src/main/java/com/eightyeightdays/jobs_au_backend/geocode/place/dto/Address.java
+++ src/main/java/com/eightyeightdays/jobs_au_backend/geocode/place/dto/Address.java
@@ -2,6 +2,8 @@ package com.eightyeightdays.jobs_au_backend.geocode.place.dto;
 
 import lombok.Getter;
 
+import java.util.Arrays;
+
 @Getter
 public class Address {
 
@@ -28,11 +30,13 @@ public class Address {
         }
 
         // suburb + state + postcode
-        String[] location = parts[1].trim().split(" ");
+        String[] location = parts[1].trim().split("\\s+");
 
-        dto.suburb = location[0];
-        dto.state = location[1];
-        dto.postcode = location[2];
+        dto.postcode = location[location.length - 1];
+        dto.state = location[location.length - 2];
+
+        // suburb는 나머지 전체
+        dto.suburb = String.join(" ", Arrays.copyOfRange(location, 0, location.length - 2));
 
         return dto;
     }
